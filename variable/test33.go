9f687e3a840377a1f9f266acdc9a87ff3e8a3250
Kwin <kwin@promote.cache-dns.local>
2017-09-25 00:10:06 +0800

    upodoate

---
+++ variable/test33.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+    //"sort"
+)
+
+func main() {
+
+    D()
+    E()
+    F()
+
+    fmt.Println("Hello world!");
+}
+
+
+func D(){
+    fmt.Println("func D");
+}
+
+func E(){
+    //使用 recover 是可以做恢复操作的
+    defer func() {
+        if err := recover();err != nil{
+            fmt.Println(err);
+            fmt.Println("Recover in E")
+        }
+    }();
+    //panic 相当于 exit,马上终端执行
+    panic("Panic in E")
+}
+
+func F(){
+    fmt.Println("Func F")
+}
