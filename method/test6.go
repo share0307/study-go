1336c0c68ce8b22e7cc9bc2b593df5edce928de5
Kwin <kwin@promote.cache-dns.local>
2017-10-06 17:43:52 +0800

    update

---
+++ method/test6.go
@@ -0,0 +1,24 @@
+package main;
+
+import "fmt";
+
+type TT int
+
+func (tt *TT)Increase(num int) {
+    //http://blog.csdn.net/sybnfkn040601/article/details/54614798
+    fmt.Println(tt)
+    //这时候已经改变了原值，因为是引用传递
+    //注意：就算TT 是 int 类型的别名，可是因为必须是相同类型才能作对比以及运算，所以还是得把 int 类型强转为 TT 类型
+    *tt += TT(num);
+    fmt.Println(*tt)
+}
+
+func main()  {
+    t := TT(10);
+
+
+    t.Increase(100);
+
+    fmt.Println("Hello World!");
+}
+
---
+++ method/test7.go
@@ -0,0 +1,18 @@
+package main;
+
+import "fmt";
+
+
+func main()  {
+    var a int;
+    var b float32;
+    a = 10;
+    b = 20;
+
+    //int + float32是错误的，因为类型不同，go 主张显示转换
+    //fmt.Println(a + b);
+    fmt.Println(float32(a) + b);
+
+    fmt.Println("Hello World!");
+}
+
