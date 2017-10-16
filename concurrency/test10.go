60b2c98635eb60aafbdc1553bb44be60b64b5105
Kwin <kwin@promote.cache-dns.local>
2017-10-16 23:57:01 +0800

    并发理解

--- concurrency/test10.go
+++ concurrency/test10.go
@@ -6,24 +6,57 @@ import (
 )
 
 func main()  {
+    //创建一个用于防止超时的频道
     timeout := make (chan bool, 1)
+    //创建一个 go routine
     go func() {
-        time.Sleep(2 * time.Second) // sleep one second
+        //睡眠1秒
+        time.Sleep(1 * time.Second) // sleep one second
+        //通过 timeout channel 发送 true
         timeout <- true
     }()
-    ch := make (chan int)
-    //close(ch)
 
-    for {
-        select {
-        case v,ok := <- ch:
-            fmt.Println(v,ok)
-        case <- timeout:
-            fmt.Println("timeout!")
-            close(ch)
-            close(timeout)
+    //创建一个 ch 的频道，用于数字传输
+    ch := make (chan int)
+
+    go func() {
+        for {
+            fmt.Println("Hello World!");
+            select {
+            case v, ok := <-ch:
+                if !ok {
+                    break;
+                }
+                fmt.Println(v, ok)
+                break;
+            case <-timeout:
+                fmt.Println("timeout!")
+                break;
+            }
         }
+    }()
+
+    //闭包..超时调用模拟定时调用
+    var fc func();
+    fc = func() {
+        time.AfterFunc(1 * time.Second, func() {
+            timeout <- true
+
+            fc();
+        });
     }
+
+    fc();
+
+
+    //close(timeout)
+
+
+    time.Sleep(10 * time.Second);
+
+
+    close(ch)
+    close(timeout)
 
 }
 
--- concurrency/test9.go
+++ concurrency/test9.go
@@ -11,14 +11,19 @@ func main()  {
     //用于信号发送
     o := make(chan bool)
 
+
     go func() {
         for {
+            //http://blog.csdn.net/zhonglinzhang/article/details/45913443
+            //此处为了验证在 for 循环(无线循环)中，需要至少从 select 中执行一个才能继续下一个循环
+            //每次 for 循环只会执行其中一个 case 或者 default，要是需要监听的队列同时发送数据过来，其实也是等执行完一个再在下一次 select 循环中执行
             select {
                 case v,ok := <-c1:
                     fmt.Println("c1");
                     if !ok{
                         //当 c1或者 c2任一有一个关闭的，那么给发送关闭信号
                         o <- true
+                        //其实这个 timeout 在 select 中没什么用
                         break
                     }
                     fmt.Println("c1:",v)
