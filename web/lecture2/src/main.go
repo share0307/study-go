73b2b3bba861b2601a7aa7b8790d696cd227e63d
Kwin <kwin@promote.cache-dns.local>
2017-10-25 23:54:02 +0800

    update

--- web/lecture2/src/main.go
+++ web/lecture2/src/main.go
@@ -1,3 +1,17 @@
-package src
+package main
 
-import "github.com/astaxie/beego/orm"
+import "github.com/astaxie/beego"
+
+type HomeController struct {
+    beego.Controller
+}
+
+func (this *HomeController) get()  {
+    this.Ctx.WriteString("Hello World!")
+}
+
+func main()  {
+    beego.Router("/",&HomeController{})
+
+    beego.Run();
+}
