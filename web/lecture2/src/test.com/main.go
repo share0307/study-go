13ca63c459fba926c2e3b0179d5d4a4d3d14cf19
Kwin <kwin@promote.cache-dns.local>
2017-10-26 00:41:13 +0800

    一个绝大的坑！

--- web/lecture2/src/test.com/main.go
+++ web/lecture2/src/test.com/main.go
@@ -1,5 +1,7 @@
 package main
 
+//http://blog.csdn.net/lastsweetop/article/details/78185713?locationNum=5&fps=1
+//https://studygolang.com/articles/10347
 import "github.com/astaxie/beego"
 
 type HomeController struct {
