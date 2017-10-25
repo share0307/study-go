9cbec7a5bf65d9d164dba60879a3d8ac0863b3b6
Kwin <kwin@promote.cache-dns.local>
2017-10-26 00:41:08 +0800

    一个绝大的坑！

---
+++ web/lecture2/src/test.com/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/astaxie/beego"
+
+type HomeController struct {
+    beego.Controller
+}
+
+func (this *HomeController) get()  {
+    this.Ctx.WriteString("Hello World!")
+}
+
+func main()  {
+    beego.Router("/",&HomeController{})
+
+    beego.Run();
+}
