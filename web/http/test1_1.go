afb63721d779aa6e266bb7c748fc5b1e8d720736
kkk <kkk@kkk.com>
2017-10-30 17:35:38 +0800

    update

---
+++ web/http/test1_1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"io"
+	"log"
+)
+
+func main()  {
+	http.HandleFunc("/", func(w http.ResponseWriter,r *http.Request) {
+		io.WriteString(w,"Hello world!");
+	});
+
+	log.Fatal(http.ListenAndServe(":8080",nil))
+
+}
