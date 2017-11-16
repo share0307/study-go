2a3785f945625939f02f79bb062a0a819f0de1b2
Kwin <kwin@promote.cache-dns.local>
2017-11-16 23:29:32 +0800

    update

--- web/lecture2/src/hello.com/routers/router.go
+++ web/lecture2/src/hello.com/routers/router.go
@@ -6,5 +6,18 @@ import (
 )
 
 func init() {
+    //首页
     beego.Router("/", &controllers.MainController{})
+    //博客首页
+    beego.Router("/blog", &controllers.BlogController{})
+    //登录
+    //beego.Router("/user", &controllers.LoginController{})
+    //退出
+    //beego.NewNamespace("/user",)
+
+
+    //自动匹配
+    beego.AutoRouter(&controllers.UserController{})
+    //自动匹配
+    beego.AutoRouter(&controllers.TestController{})
 }
